9d20c4b484c8bf68631f3c2d7263f499b041b119
Ewen Gougis | UnderTrack <undertracker.mc@gmail.com>
2018-02-10 20:40:56 +0400

    Added Simple Math and Reset Fonction

--- Sluchaynaya/Computer.cs
+++ Sluchaynaya/Computer.cs
@@ -8,26 +8,26 @@ namespace Sluchaynaya
 {
 	class PCMain
 	{
-		static void Main()
+		public static void Main()
 		{
 			//Initializing a memory
 			Memory MEM = new Memory();
-			MEM.AX = new byte[4] { 0, 0, 0, 0 };
+			MEM.AX = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+			MEM.CX = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+			MEM.DX = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+			MEM.SI =  new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+			MEM.SP = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+			MEM.BP = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+			MEM.CS = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+			MEM.DS = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+			MEM.SS = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+			MEM.ES = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+			MEM.IP = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
 			MEM.BX = 0;
-			MEM.CX = new byte[4] { 0, 0, 0, 0 };
-			MEM.DX = new byte[4] { 0, 0, 0, 0 };
-			MEM.SI = new byte[4] { 0, 0, 0, 0 };
 			MEM.DI = 0;
-			MEM.SP = new byte[4] { 0, 0, 0, 0 };
-			MEM.BP = new byte[4] { 0, 0, 0, 0 };
-			MEM.CS = new byte[4] { 0, 0, 0, 0 };
-			MEM.DS = new byte[4] { 0, 0, 0, 0 };
-			MEM.SS = new byte[4] { 0, 0, 0, 0 };
-			MEM.ES = new byte[4] { 0, 0, 0, 0 };
 			MEM.FS = 0;
 			MEM.GS = 0;
 			MEM.FLAG = 0;
-			MEM.IP = new byte[4] { 0, 0, 0, 0 };
 			MEM.DG = 0;
 			MEM.EP = 0;
 
@@ -35,6 +35,7 @@ namespace Sluchaynaya
 			Console.WriteLine("Hello World !");
 
 			//Initialising data for multiplication
+			Console.WriteLine();
 			Console.WriteLine("Initializing Bytes :");
 			MEM.SI[0] = 2;
 			MEM.SI[1] = 3;
@@ -48,6 +49,7 @@ namespace Sluchaynaya
 			Console.WriteLine("SI : {0} ", BitConverter.ToString(MEM.SI));
 
 			//Initialising data for division
+			Console.WriteLine();
 			Console.WriteLine("Initializing Bytes :");
 			MEM.SI[2] = 10;
 			MEM.SI[3] = 5;
@@ -60,8 +62,45 @@ namespace Sluchaynaya
 			Console.WriteLine("AX : {0} ", BitConverter.ToString(MEM.AX));
 			Console.WriteLine("SI : {0} ", BitConverter.ToString(MEM.SI));
 
+			//Initialising data for addition
+			Console.WriteLine();
+			Console.WriteLine("Initializing Bytes :");
+			MEM.SI[4] = 2;
+			MEM.SI[5] = 2;
+			//Debug
+			Console.WriteLine("AX : {0} ", BitConverter.ToString(MEM.AX));
+			Console.WriteLine("SI : {0} ", BitConverter.ToString(MEM.SI));
+			//Dividing
+			Fonctions.Add(MEM);
+			//Debug
+			Console.WriteLine("AX : {0} ", BitConverter.ToString(MEM.AX));
+			Console.WriteLine("SI : {0} ", BitConverter.ToString(MEM.SI));
+
+			//Initialising data for substraction
+			Console.WriteLine();
+			Console.WriteLine("Initializing Bytes :");
+			MEM.SI[6] = 10;
+			MEM.SI[7] = 5;
+			//Debug
+			Console.WriteLine("AX : {0} ", BitConverter.ToString(MEM.AX));
+			Console.WriteLine("SI : {0} ", BitConverter.ToString(MEM.SI));
+			//Dividing
+			Fonctions.Substract(MEM);
+			//Debug
+			Console.WriteLine("AX : {0} ", BitConverter.ToString(MEM.AX));
+			Console.WriteLine("SI : {0} ", BitConverter.ToString(MEM.SI));
+
+			//Reset
+			Fonctions.Reset.SI(MEM);
+			Console.WriteLine("Memory Bytes :");
+			Console.WriteLine("AX : {0} ", BitConverter.ToString(MEM.AX));
+			Console.WriteLine("SI : {0} ", BitConverter.ToString(MEM.SI));
+
 			//Clean-up
-			Console.SetCursorPosition(0, Console.WindowHeight - 1);
+			System.Threading.Thread.Sleep(1000);
+			Console.WriteLine();
+			Console.WriteLine("Press any key to continue... ");
+			Console.ReadLine();
 		}
 	}
 }
--- Sluchaynaya/Fonctions.cs
+++ Sluchaynaya/Fonctions.cs
@@ -9,6 +9,7 @@ namespace Sluchaynaya
 	{
 		public static void Multiply(Memory Memory)
 		{
+			Console.WriteLine();
 			Console.WriteLine("Multiplying...");
 			byte A = Memory.SI[0];
 			byte B = Memory.SI[1];
@@ -16,6 +17,7 @@ namespace Sluchaynaya
 			_R = A * B;
 			Memory.AX[0] = BitConverter.GetBytes(_R)[0];
 			Console.WriteLine("{0} * {1} = {2}", Memory.SI[0], Memory.SI[1], Memory.AX[0]);
+			Console.WriteLine();
 			/*Debug
 			string RDebug = Convert.ToString(_R);
 			Console.Write("Value sent to memory : ");
@@ -23,6 +25,7 @@ namespace Sluchaynaya
 		}
 		public static void Divide(Memory Memory)
 		{
+			Console.WriteLine();
 			Console.WriteLine("Dividing...");
 			byte A = Memory.SI[2];
 			byte B = Memory.SI[3];
@@ -30,10 +33,140 @@ namespace Sluchaynaya
 			_R = A / B;
 			Memory.AX[1] = BitConverter.GetBytes(_R)[0];
 			Console.WriteLine("{0} / {1} = {2}", Memory.SI[2], Memory.SI[3], Memory.AX[1]);
+			Console.WriteLine();
 			/*Debug
 			string RDebug = Convert.ToString(_R);
 			Console.Write("Value sent to memory : ");
 			Console.WriteLine(RDebug);*/
+		}
+		public static void Add(Memory Memory)
+		{
+			Console.WriteLine();
+			Console.WriteLine("Adding...");
+			byte A = Memory.SI[4];
+			byte B = Memory.SI[5];
+			int _R;
+			_R = A + B;
+			Memory.AX[2] = BitConverter.GetBytes(_R)[0];
+			Console.WriteLine("{0} + {1} = {2}", Memory.SI[4], Memory.SI[5], Memory.AX[2]);
+			Console.WriteLine();
+			/*Debug
+			string RDebug = Convert.ToString(_R);
+			Console.Write("Value sent to memory : ");
+			Console.WriteLine(RDebug);*/
+		}
+		public static void Substract(Memory Memory)
+		{
+			Console.WriteLine();
+			Console.WriteLine("Substracting...");
+			byte A = Memory.SI[6];
+			byte B = Memory.SI[7];
+			int _R;
+			_R = A - B;
+			Memory.AX[3] = BitConverter.GetBytes(_R)[0];
+			Console.WriteLine("{0} - {1} = {2}", Memory.SI[6], Memory.SI[7], Memory.AX[3]);
+			Console.WriteLine();
+			/*Debug
+			string RDebug = Convert.ToString(_R);
+			Console.Write("Value sent to memory : ");
+			Console.WriteLine(RDebug);*/
+		}
+		public class Reset
+		{
+			public static void AX(Memory Memory)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Reseting AX...");
+				Memory.AX = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Console.WriteLine("Reset completed...");
+			}
+			public static void CX(Memory Memory)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Reseting CX...");
+				Memory.CX = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Console.WriteLine("Reset completed...");
+			}
+			public static void DX(Memory Memory)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Reseting DX...");
+				Memory.DX = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Console.WriteLine("Reset completed...");
+			}
+			public static void SI(Memory Memory)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Reseting SI...");
+				Memory.SI = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Console.WriteLine("Reset completed...");
+			}
+			public static void SP(Memory Memory)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Reseting SP...");
+				Memory.SP = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Console.WriteLine("Reset completed...");
+			}
+			public static void BP(Memory Memory)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Reseting BP...");
+				Memory.BP = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Console.WriteLine("Reset completed...");
+			}
+			public static void CS(Memory Memory)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Reseting CS...");
+				Memory.CS = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Console.WriteLine("Reset completed...");
+			}
+			public static void DS(Memory Memory)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Reseting DS...");
+				Memory.DS = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Console.WriteLine("Reset completed...");
+			}
+			public static void SS(Memory Memory)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Reseting SS...");
+				Memory.SS = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Console.WriteLine("Reset completed...");
+			}
+			public static void ES(Memory Memory)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Reseting ES...");
+				Memory.ES = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Console.WriteLine("Reset completed...");
+			}
+			public static void IP(Memory Memory)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Reseting IP...");
+				Memory.IP = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Console.WriteLine("Reset completed...");
+			}
+			public static void All(Memory Memory)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Reseting all Memory byte arrays...");
+				Memory.AX = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Memory.CX = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Memory.DX = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Memory.SI =  new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Memory.SP = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Memory.BP = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Memory.CS = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Memory.DS = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Memory.SS = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Memory.ES = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Memory.IP = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+				Console.WriteLine("Reset completed...");
+			}
 		}
 	}
 }
